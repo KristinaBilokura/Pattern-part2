659f78b8c37b6423d4432949d51523a6c319dd1c
bilokura <kristinabilokura@gmail.com>
2017-12-05 23:24:01 +0200

    add methods(specialized)

--- src/main/java/epam/lab/Prototype/main.java
+++ src/main/java/epam/lab/Prototype/main.java
@@ -6,12 +6,14 @@ public class main {
 
             Monsters clonedmonster1 = (Monsters) MonstersRepository.getName("Zoozoo");
             System.out.println("Monster : " + clonedmonster1.getType());
+            clonedmonster1.battle();
 
             Monsters clonedmonster2 = (Monsters) MonstersRepository.getName("Drakula");
             System.out.println("Monster : " + clonedmonster2.getType());
-
+            clonedmonster2.defence();
             Monsters clonedmonster3 = (Monsters) MonstersRepository.getName("Poltergeist");
             System.out.println("Monster : " + clonedmonster3.getType());
+            clonedmonster3.battle();
         }
 
 }
